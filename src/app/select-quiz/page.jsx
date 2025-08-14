c79a694d31266e572b976630c30e93d26caa799c
Juan Pablo <cab-18@hotmail.com>
2025-08-13 22:44:27 -0500

    7

--- src/app/select-quiz/page.jsx
+++ src/app/select-quiz/page.jsx
@@ -6,7 +6,7 @@ export default function SelectQuiz() {
     return (
         <div className="flex flex-col justify-center items-center mt-14">
             <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
-                Select <AuroraText>Course: </AuroraText>
+                Seleccione <AuroraText>Curso: </AuroraText>
             </h1>
             <QuizCards />
         </div>
